f745ec0f9444720ae1d377b3a71f1e7beb69ca24
Geoffrey Foster <geoffrey.foster@gmail.com>
2012-02-26 23:21:58 -0500

    commands now support blocks

--- Campfire/Source/GFCampfireCommand.h
+++ Campfire/Source/GFCampfireCommand.h
@@ -13,9 +13,12 @@
 @property (readonly, strong) NSString *command;
 @property (readonly, strong) NSString *helpString;
 @property (readonly, assign) SEL selector;
+@property (readonly, copy) void (^block)(NSString *args);
 
 + (GFCampfireCommand *)commandWithName:(NSString *)command helpString:(NSString *)helpString selector:(SEL)aSelector;
++ (GFCampfireCommand *)commandWithName:(NSString *)command helpString:(NSString *)helpString action:(void (^)(NSString *args))block;
 - (instancetype)initWithName:(NSString *)command helpString:(NSString *)helpString selector:(SEL)aSelector;
+- (instancetype)initWithName:(NSString *)command helpString:(NSString *)helpString action:(void (^)(NSString *args))aBlock;
 
 - (void)performActionWithObject:(id)obj args:(NSString *)args;
 
--- Campfire/Source/GFCampfireCommand.m
+++ Campfire/Source/GFCampfireCommand.m
@@ -13,6 +13,7 @@
 @property (readwrite, strong) NSString *command;
 @property (readwrite, strong) NSString *helpString;
 @property (readwrite, assign) SEL selector;
+@property (readwrite, copy) void (^block)(NSString *params);
 
 @end
 
@@ -21,10 +22,16 @@
 @synthesize command;
 @synthesize helpString;
 @synthesize selector;
+@synthesize block;
 
 + (GFCampfireCommand *)commandWithName:(NSString *)command helpString:(NSString *)helpString selector:(SEL)aSelector
 {
 	return [[self alloc] initWithName:command helpString:helpString selector:aSelector];
+}
+
++ (GFCampfireCommand *)commandWithName:(NSString *)command helpString:(NSString *)helpString action:(void (^)(NSString *args))block
+{
+	return [[self alloc] initWithName:command helpString:helpString action:block];
 }
 
 - (instancetype)initWithName:(NSString *)aCommand helpString:(NSString *)aHelpString selector:(SEL)aSelector
@@ -38,10 +45,25 @@
 	return self;
 }
 
+- (instancetype)initWithName:(NSString *)aCommand helpString:(NSString *)aHelpString action:(void (^)(NSString *args))aBlock
+{
+	if ((self = [super init])) {
+		self.command = aCommand;
+		self.helpString = aHelpString;
+		self.block = aBlock;
+	}
+	
+	return self;
+}
+
 - (void)performActionWithObject:(id)obj args:(NSString *)args
 {
 	args = [args stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceCharacterSet]];
-	[obj performSelector:self.selector withObject:args];
+	if (self.selector) {
+		[obj performSelector:self.selector withObject:args];
+	} else if (self.block) {
+		self.block(args);
+	}
 }
 
 @end
